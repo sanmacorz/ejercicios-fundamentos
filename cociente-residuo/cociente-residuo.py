6e874ddb56a571c9be1a55a9c857eb58bb64a360
Santiago Macías <santiagomacias8@hotmail.com>
2022-04-05 15:26:11 -0500

    Añadiendo los archivos iniciales

---
+++ cociente-residuo/cociente-residuo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Ejercicio 1. Crear un programa para sumar dos números enteros.
+
+# Define la variable "numero1" como un entero (int) y crea una input para preguntar por el valor deseado de este número. (INPUT-STORAGE)
+x = int(input("Introduzca un numero: "))
+# Define la variable "numero1" como un entero (int) y crea una input para preguntar por el valor deseado de este número. (INPUT-STORAGE)
+z1 = (x / y)
+# Define la variable "numero1" como un entero (int) y crea una input para preguntar por el valor deseado de este número. (INPUT-STORAGE)
+z2 = (x % y)
+# Define la variable "numero1" como un entero (int) y crea una input para preguntar por el valor deseado de este número. (INPUT-STORAGE)
+z3 = (x // y)
+# Define la variable "numero1" como un entero (int) y crea una input para preguntar por el valor deseado de este número. (INPUT-STORAGE)
+print("La división es: " + str(z1) + "\n El módulo es: " + str(z2) + "\n La división como entero es: " + str(z3))
